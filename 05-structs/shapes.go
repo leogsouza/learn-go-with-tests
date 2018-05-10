8a2103ab59cf7e77fb38cc5eb8b06077f70f5081
Leonardo Souza <leogsouza@gmail.com>
2018-05-10 21:03:06 +0000

    Refactor - Structs more clearly on tests

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -33,11 +33,13 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+// Triangle represents the area of a triangle
 type Triangle struct {
 	Base   float64
 	Height float64
 }
 
+// Area returns the area of a triangle
 func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) * 0.5
 }
