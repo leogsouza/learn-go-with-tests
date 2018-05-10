b0ae2456b59ed74debf54e0e2642c5d1604a67b4
Leonardo Souza <leogsouza@gmail.com>
2018-05-10 20:20:17 +0000

    Add Shape Interface

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -2,24 +2,33 @@ package structs
 
 import "math"
 
+// Shape is implemented by anithing that can tell us its area
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle represents a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-// Perimeter calculates the perimeter of a shape
+// Perimeter calculates the perimeter of rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
+// Area calculates the area of a rectangle
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+// Circle represents a circle
 type Circle struct {
 	Radius float64
 }
 
+// Area returns the area of a circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
