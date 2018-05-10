373346ef4d29192a7e2498b0d052122a211e8ac1
Leonardo Souza <leogsouza@gmail.com>
2018-05-10 19:37:22 +0000

    Add Circle and Rectangle structs

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -1,10 +1,25 @@
 package structs
 
-// Perimeter calculates the perimeter of a shape
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+// Perimeter calculates the perimeter of a shape
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
