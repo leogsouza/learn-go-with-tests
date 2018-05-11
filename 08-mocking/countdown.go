16546e920498b3da837f57276425bee36a34db33
Leonardo Souza <leogsouza@gmail.com>
2018-05-11 21:04:31 +0000

    Create struct to simulate calls of time.Sleep

--- 08-mocking/countdown.go
+++ 08-mocking/countdown.go
@@ -10,17 +10,37 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 
 	}
-	time.Sleep(1 * time.Second)
-	fmt.Fprintln(out, finalWord)
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }
