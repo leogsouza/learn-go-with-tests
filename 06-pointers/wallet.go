e4096a523b81f38d61c2ecc0ffff5093dd3c9fe0
Leonardo Souza <leogsouza@gmail.com>
2018-05-11 13:39:01 +0000

    Create Bitcoin type as int

--- 06-pointers/wallet.go
+++ 06-pointers/wallet.go
@@ -1,16 +1,27 @@
 package pointers
 
+import (
+	"fmt"
+)
+
+// Biticoin represents the amount of bitcoins
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet represents a wallet of bitcoins
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit increase amount of cryptocurrency
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance returns total of amount
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
