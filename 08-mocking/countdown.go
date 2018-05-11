b297a3339b89ee749b3986a7783cf47d26e20d87
Leonardo Souza <leogsouza@gmail.com>
2018-05-11 21:25:45 +0000

    Mocking more structs

--- 08-mocking/countdown.go
+++ 08-mocking/countdown.go
@@ -30,6 +30,23 @@ func (o *ConfigurableSleeper) Sleep() {
 	time.Sleep(o.duration)
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
