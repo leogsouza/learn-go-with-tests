4fb22ba11ebafb820a22dc48c7898f4de353e24a
Leonardo Souza <leogsouza@gmail.com>
2018-05-11 13:56:19 +0000

    Add Withdraw feature and refactor tests

--- 06-pointers/wallet_test.go
+++ 06-pointers/wallet_test.go
@@ -3,14 +3,34 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	checkBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		want := Bitcoin(10)
+		checkBalance(t, wallet, want)
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		want := Bitcoin(10)
+
+		checkBalance(t, wallet, want)
+
+	})
+
 }
